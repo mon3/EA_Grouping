3c2b12978a19c6eae7c7e625b9bf4d644bbfa5d4
mon3 <monika.seniut@gmail.com>
2017-06-04 13:43:21 +0200

    added groupingRes extraction from resultMaster for PAM, Hclust and kmeans with appropriate metric

--- testfield.R
+++ testfield.R
@@ -79,6 +79,83 @@ cutAverageResHclust <- function(resultMaster){
   
 }
 
+cutAverageResPam <- function(resultMaster){
+  hscores = list()
+  
+  for(i in 6:15){
+    singlescore = list()
+    singlescoreMan = list()
+    for(j in seq(1,50)){
+      singlescore[[j]] <- list()
+      singlescore[[j]]$dunn <- 0
+      singlescore[[j]]$sil <- 0
+      singlescore[[j]]$gind <- 0
+      singlescore[[j]]$k <- 0
+  
+      singlescoreMan[[j]] <- list()
+      singlescoreMan[[j]]$dunn <- 0
+      singlescoreMan[[j]]$sil <- 0
+      singlescoreMan[[j]]$gind <- 0
+      singlescoreMan[[j]]$k <- 0
+      for(k in 1:10){
+        singlescore[[j]]$dunn <- singlescore[[j]]$dunn + resultMaster[[i]]$hclust$euclidean[[k]][[j]]$dunn
+        singlescore[[j]]$sil <- singlescore[[j]]$sil + resultMaster[[i]]$hclust$euclidean[[k]][[j]]$sil
+        singlescore[[j]]$gind <- singlescore[[j]]$gind+ resultMaster[[i]]$hclust$euclidean[[k]][[j]]$gind
+        singlescore[[j]]$k <- singlescore[[j]]$k + resultMaster[[i]]$hclust$euclidean[[k]][[j]]$k
+        
+        singlescoreMan[[j]]$dunn <- singlescoreMan[[j]]$dunn + resultMaster[[i]]$hclust$manhattan[[k]][[j]]$dunn
+        singlescoreMan[[j]]$sil <- singlescoreMan[[j]]$sil + resultMaster[[i]]$hclust$manhattan[[k]][[j]]$sil
+        singlescoreMan[[j]]$gind <- singlescoreMan[[j]]$gind+ resultMaster[[i]]$hclust$manhattan[[k]][[j]]$gind
+        singlescoreMan[[j]]$k <- singlescoreMan[[j]]$k + resultMaster[[i]]$hclust$manhattan[[k]][[j]]$k
+      }
+      singlescore[[j]]$dunn <- singlescore[[j]]$dunn/10
+      singlescore[[j]]$sil <- singlescore[[j]]$sil/10
+      singlescore[[j]]$gind <- singlescore[[j]]$gind/10
+      singlescore[[j]]$k <- singlescore[[j]]$k/10
+      
+      singlescoreMan[[j]]$dunn <- singlescoreMan[[j]]$dunn/10
+      singlescoreMan[[j]]$sil <- singlescoreMan[[j]]$sil/10
+      singlescoreMan[[j]]$gind <- singlescoreMan[[j]]$gind/10
+      singlescoreMan[[j]]$k <- singlescoreMan[[j]]$k/10
+    }
+    hscores[[i]] <- list(euclidean=singlescore, manhattan=singlescoreMan)
+  }
+  return(hscores)
+  
+}
+
+
+cutAverageResKmeans <- function(resultMaster){
+  hscores = list()
+  
+  for(i in 6:15){
+    singlescore = list()
+    singlescoreMan = list()
+    for(j in seq(1,50)){
+      singlescore[[j]] <- list()
+      singlescore[[j]]$dunn <- 0
+      singlescore[[j]]$sil <- 0
+      singlescore[[j]]$gind <- 0
+      singlescore[[j]]$k <- 0
+      
+      for(k in 1:10){
+        singlescore[[j]]$dunn <- singlescore[[j]]$dunn + resultMaster[[i]]$hclust$euclidean[[k]][[j]]$dunn
+        singlescore[[j]]$sil <- singlescore[[j]]$sil + resultMaster[[i]]$hclust$euclidean[[k]][[j]]$sil
+        singlescore[[j]]$gind <- singlescore[[j]]$gind+ resultMaster[[i]]$hclust$euclidean[[k]][[j]]$gind
+        singlescore[[j]]$k <- singlescore[[j]]$k + resultMaster[[i]]$hclust$euclidean[[k]][[j]]$k
+
+      }
+      singlescore[[j]]$dunn <- singlescore[[j]]$dunn/10
+      singlescore[[j]]$sil <- singlescore[[j]]$sil/10
+      singlescore[[j]]$gind <- singlescore[[j]]$gind/10
+      singlescore[[j]]$k <- singlescore[[j]]$k/10
+    }
+    hscores[[i]] <- list(euclidean=singlescore)
+  }
+  return(hscores)
+  
+}
+
 
 gaSavePopulation <- function(obj, name){
   env = globalenv()
